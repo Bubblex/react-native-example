39ce4718bcfe4da0b1050178af4f676e33303a62
Bubblex <407505475@qq.com>
2017-10-09 10:51:03 +0800

    使用  react-navigation

--- App.js
+++ App.js
@@ -1,34 +1,17 @@
 import React from 'react';
-import { Alert } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button } from 'antd-mobile';
+import { StyleSheet, Text, View } from 'react-native'
+import { StackNavigator } from 'react-navigation'
+import { Button } from 'antd-mobile'
+import UserScreen from './src/user'
+import HomeScreen from './src/main'
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+  User: { screen: UserScreen },
+});
 
 export default class App extends React.Component {
-  _handleButtonPress = () => {
-    Alert.alert(
-      'Button pressed!',
-      'You did it!',
-    )
-  }
-
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello world!</Text>
-        <Button  
-          type='primary'
-          onClick={this._handleButtonPress}
-        >alert</Button>
-      </View>
-    );
+    return <SimpleApp />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
